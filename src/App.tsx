ae2c97105b46e5c47e14e664a839cd3e0ccd32c9
DoxedN7 <doxed97@protonmail.com>
2020-12-29 16:15:34 +0100

    Ajout premiere card

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
+  //IonIcon,
+  //IonLabel,
   IonRouterOutlet,
   IonTabBar,
-  IonTabButton,
+  //IonTabButton,
   IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+//import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -45,18 +45,7 @@ const App: React.FC = () => (
           <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
+     <p>salut</p>
         </IonTabBar>
       </IonTabs>
     </IonReactRouter>
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
+
+    
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+        <IonToolbar color="primary">
+          <IonTitle>Vaches</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Vache n°1</IonCardTitle>
+        </IonCardHeader>
+        <img src="assets/cow_1.jpg"/>
+        <IonCardContent>
+          Meuh
+        </IonCardContent>
+      </IonCard>
+
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large"></IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
